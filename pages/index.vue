eaedd372f299039725d4cdeffe196ace08d370f1
lassespilling <lassespilling@gmail.com>
2020-02-06 14:52:17 +0100

    Add button styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h1>Lasse D. Spilling</h1>
-    <a href="https://vg.no">Read newspaper</a>
+    <a href="https://vg.no" class="vg">Read newspaper</a>
   </div>
 </template>
 
@@ -23,6 +23,26 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction: column;
+}
+
+.vg {
+  letter-spacing: 0.05rem;
+  margin: 1rem;
+  display: block;
+  transform: scale(.9);
+  transition: all .2s ease;
+  background: red;
+  font-family: system-ui;
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+  padding: .5rem 1rem;
+  border-radius: 1rem;
+}
+.vg:hover {
+  transform: scale(1);
+  letter-spacing: 0.1rem;
 }
 
 .title {
